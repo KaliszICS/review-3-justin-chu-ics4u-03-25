875a03e570ac920961512ab8703efc138b14d7ed
justin-chu-ics4u-03-25 <justin.chu4@student.tdsb.on.ca>
2025-02-06 20:12:15 +0000

    My answers yuh

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -13,37 +13,84 @@ public class PracticeProblem {
 	}
 
 	public static void q1() {
-		//Write question 1 code here
+		Scanner scanner = new Scanner(System.in);
+
+		System.out.print("In: ");
+		String sussybaka = scanner.nextLine();
+
+		System.out.println(sussybaka.charAt(0));
 		
 	}
 
 	public static void q2() {
-		//Write question 2 code here
+		Scanner scanner = new Scanner(System.in);
+
+		System.out.print("In: ");
+		boolean bean = scanner.nextBoolean();
+		scanner.nextLine();
+
+		System.out.println(!bean);
 		
 	}
 
 	public static void q3() {
-		//Write question 3 code here
+		Scanner scanner = new Scanner(System.in);
+
+		System.out.print("In: ");
+		int intagar = scanner.nextInt();
+		scanner.nextLine();
+
+		System.out.println(intagar > 5);
 		
 	}
 
 	public static void q4() {
-		//Write question 4 code here
+		Scanner scanner = new Scanner(System.in);
 		
+		System.out.print("In: ");
+		double victoryroyale = scanner.nextDouble();
+		scanner.nextLine();
+
+		System.out.println((victoryroyale >= -2) && (victoryroyale <=2));
 	}
 
 	public static void q5() {
-		//Write question 5 code here
+		Scanner scanner = new Scanner(System.in);
+
+		System.out.print("In: ");
+		String jackieweng = scanner.nextLine();
+
+		System.out.println("Hello World".equals(jackieweng));
 		
 	}
 
 	public static void q6() {
-		//Write question 6 code here
+		Scanner scanner = new Scanner(System.in);
+
+		System.out.print("In: ");
+		int wan = scanner.nextInt();
+		scanner.nextLine();
+
+		System.out.print("In: ");
+		int too = scanner.nextInt();
+		scanner.nextLine();
+
+		System.out.println(wan <= too);
 		
 	}
 
 	public static void q7() {
-		//Write question 7 code here
+		Scanner scanner = new Scanner(System.in);
+
+		System.out.print("In: ");
+		double dubba = scanner.nextDouble();
+		scanner.nextLine();
+
+		System.out.print("In: ");
+		double dooba = scanner.nextDouble();
+		scanner.nextLine();
+		
+		System.out.println(dubba > dooba);
 		
 	}
 
